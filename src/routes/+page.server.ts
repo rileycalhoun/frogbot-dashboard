c0e801c1f0a998354ed29ec6e9df10e036efb5bd
Riley Calhoun <rileycalhoun0228@gmail.com>
2023-08-12 17:26:12 -0700

    Fix Timezone

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,18 @@
 import type { PageServerLoad } from './$types';
 import { ORIGIN, DISCORD_REDIRECT_PATH } from '$env/static/private';
 
-const days = ['Sunday', 'Monday', 'Tuesday', 
+const days = ['Sunday', 'Monday', 'Tuesday',
 	'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const redirect_uri = `${ORIGIN}${DISCORD_REDIRECT_PATH}`;
 
 export const load: PageServerLoad = async ({ locals }) => {
-	let index = new Date().getDay();
-	let day = days[index];
+	let todayPST = new Date().toLocaleString('en-US', {
+		timeZone: 'America/Los_Angeles'
+	});
+
+	let index = new Date(todayPST).getDay();
+	let day = days[index as unknown as number];
 
 	return {
 		props: {
